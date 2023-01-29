e2bd20d4f85e8e5d704301877b9e0d69565dcc5c
GdonatasG <zitkusd@gmail.com>
2023-01-29 15:34:25 +0200

    init

---
+++ src/main/java/com/projectspace/projectspaceapi/ProjectSpaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.projectspace.projectspaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpaceApiApplication.class, args);
+    }
+
+}
